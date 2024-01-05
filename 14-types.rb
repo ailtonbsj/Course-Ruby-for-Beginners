17993884e34a0a30ba8482c17f8b13c6dbab4fbe
ailtonbsj <ailton.ifce@gmail.com>
2024-01-05 16:20:14 -0300

    types and strings

---
+++ 14-types.rb
@@ -0,0 +1,20 @@
+puts 101.class
+
+puts 5e100.class
+
+puts 1.01.class
+
+puts 1234567890123456789012345678901234567890123456789012345678.class
+
+puts true.class
+
+puts "Hello".class
+
+puts 0b1010
+
+puts 0o45
+
+puts 0xFF
+
+puts 1_000_000
+
---
+++ 15-strings.rb
@@ -0,0 +1,22 @@
+version = 0.1
+
+var1 = 'Version: #{version}'
+
+var2 = "Version: #{version}"
+
+puts var1
+puts var2
+puts var1.encoding
+puts var2.encoding
+
+puts "String with 'escapes' \"caracters\""
+
+multi = <<EOF
+This
+is
+a
+multiline
+string
+EOF
+
+puts multi
